ddeb7edfd59a62f40c409dea9d2014cd67d9203a
Artem Liakhov <Liakhov.Artem@yandex.ru>
2023-02-28 23:27:22 +0300

    Tested the current functionality on a simple test

--- main.cpp
+++ main.cpp
@@ -3,37 +3,14 @@
 
 
 int main() {
-    Graph<std::string, size_t> sent_valent  = {{{"Mikhail", "Olya"}, 2}, {{"Senya", "Masha"}, 1},
-                                               {{"Kostya", "Nastya"}, 6}, {{"Maxim", "Kristina"}, 3},
-                                               {{"Denis", "Liza"}, 1}, {{"Maxim", "Vika"}, 8},
-                                               {{"Denis", "Liza"}, 3}};
-    Graph<std::string, size_t> sent_valent2 = {{{"Kolya", "Natasha"}, 2}, {{"Karina", "Misha"}, 1},
-                                               {{"Liza", "Denis"}, 1}, {{"Vika", "Kostya"}, 3}};
-
-    Graph<std::string, size_t> g2 = sent_valent;
-    Graph<std::string, size_t> g3 = std::move(g2);
-
-    std::cout << "Is g2 empty? " << g2.empty() << std::endl;
-    std::cout << g3.size() << std::endl;
-
-    std::cout << std::endl;
-    std::cout << "sent_valent:" << std::endl;
-    sent_valent.print_edges();
-    std::cout << std::endl;
-    std::cout << "sent_valent2:" << std::endl;
-    sent_valent2.print_edges();
-    std::cout << std::endl;
-
-    sent_valent.swap(sent_valent2);
-    std::cout << std::endl;
-    std::cout << "after swapping:" << std::endl;
-    std::cout << std::endl;
-    std::cout << "sent_valent:" << std::endl;
-    sent_valent.print_edges();
-    std::cout << std::endl;
-    std::cout << "sent_valent2:" << std::endl;
-    sent_valent2.print_edges();
-    std::cout << std::endl;
-
+    Graph<int, int> g1;
+    std::cout << "size: " << g1.size() << std::endl;
+    std::cout << "Is empty? " << g1.empty() << std::endl;
+    g1.clear();..
+    auto beg = g1.begin();
+    auto end = g1.end();
+    auto cbeg = g1.cbegin();
+    auto cend = g1.cend();
+    std::cout << "program completed successfully" << std::endl;
     return 0;
 }
