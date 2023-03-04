6df279d41fd3c16911d53d1dfbebd6ffc936fb48
Artem Liakhov <Liakhov.Artem@yandex.ru>
2023-03-04 18:55:21 +0300

    Was checked the correctness of the return value of begin method

--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@ int main() {
     Graph<int, int> g1;
     std::cout << "size: " << g1.size() << std::endl;
     std::cout << "Is empty? " << g1.empty() << std::endl;
-    g1.clear();..
+    g1.clear();
     auto beg = g1.begin();
     auto end = g1.end();
     auto cbeg = g1.cbegin();
     auto cend = g1.cend();
     std::cout << "program completed successfully" << std::endl;
+    std::cout << cbeg->first << cbeg ->second << std::endl;
     return 0;
 }
