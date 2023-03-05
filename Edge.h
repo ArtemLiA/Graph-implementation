cf3d2ecaabd8790a028e1c18484af3106be250af
Artem Liakhov <Liakhov.Artem@yandex.ru>
2023-03-05 18:00:06 +0300

    Fixed mistake in the Edge class constructor

--- Edge.h
+++ Edge.h
@@ -8,18 +8,27 @@ protected:
     std::pair<key_type, key_type> nodes;
     weight_type weight;
 public:
-    explicit Edge(std::pair<key_type, key_type> pair, key_type w = key_type());
+    explicit Edge(std::pair<key_type, key_type> pair, weight_type w = weight_type());
+    Edge(key_type begin, key_type end, key_type w = key_type());
     std::pair<key_type, key_type> get_nodes() const;
     key_type first() const;
     key_type second() const;
     weight_type get_weight() const;
     bool is_starting_equal(key_type beg) const;
     bool is_ending_equal(key_type end) const;
+    void set_value(std::pair<key_type, key_type> pair, weight_type w);
+    void set_value(key_type begin, key_type end, weight_type w);
 };
 
 template<class weight_type, class key_type>
-Edge<weight_type, key_type>::Edge(std::pair<key_type, key_type> pair, key_type w) {
+Edge<weight_type, key_type>::Edge(std::pair<key_type, key_type> pair, weight_type w) {
     nodes = pair;
+    weight = w;
+}
+
+template<class weight_type, class key_type>
+Edge<weight_type, key_type>::Edge(key_type begin, key_type end, key_type w) {
+    nodes = std::make_pair(begin, end);
     weight = w;
 }
 
